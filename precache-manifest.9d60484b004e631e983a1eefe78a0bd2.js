931ccea8edca08f53e613af41998eec3fe7c1713
Tito-Chu <freelance.jason24608@gmail.com>
2020-07-20 12:40:49 +0800

    Updates

---
+++ precache-manifest.9d60484b004e631e983a1eefe78a0bd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2f21d3705f8b70c2ce6",
+    "url": "/React_js_HelloWorld/static/css/main.6f8fbd4c.chunk.css"
+  },
+  {
+    "revision": "a2f21d3705f8b70c2ce6",
+    "url": "/React_js_HelloWorld/static/js/main.7d9eca07.chunk.js"
+  },
+  {
+    "revision": "b608ff3931732da8ef23",
+    "url": "/React_js_HelloWorld/static/js/runtime~main.65238661.js"
+  },
+  {
+    "revision": "abc921c608ebbf2c207b",
+    "url": "/React_js_HelloWorld/static/js/2.fdc7f0fe.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_js_HelloWorld/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "165a5fe164f9f4d43fa6c2c7751b2ce7",
+    "url": "/React_js_HelloWorld/index.html"
+  }
+];
